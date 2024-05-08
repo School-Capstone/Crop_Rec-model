969d59b9e279a019c355d18de952b04e968b99af
nkusikevin <nkusikvnhart@gmail.com>
2024-05-08 12:29:25 +0200

    fix uuid

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker server:app
 init: alembic init alembic
 upgrade: alembic upgrade head
-migrate: alembic revision --autogenerate -m "New Migration4"
+migrate: alembic revision --autogenerate -m "New Migration5"
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
+import uuid
 
 Base = declarative_base()
 
